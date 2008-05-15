324803e66d7fe2f1c7209b8bb79358f2a468bdb0
nol888 <nol888@cfc62999-394d-0410-9d05-3f146b4bb521>
2008-05-15 00:12:25 +0000

    Initial code and license import.

---
+++ trunk/DemonServer/Program.cs
@@ -0,0 +1,40 @@
+﻿/*
++---------------------------------------------------------------------------+
+|	Demon - dAmn Emulator													|
+|===========================================================================|
+|	Copyright © 2008 Nol888													|
+|===========================================================================|
+|	This file is part of Demon.												|
+|																			|
+|	Demon is free software: you can redistribute it and/or modify			|
+|	it under the terms of the GNU General Public License as published by	|
+|	the Free Software Foundation, either version 3 of the License, or		|
+|	(at your option) any later version.										|
+|																			|
+|	Demon is distributed in the hope that it will be useful,				|
+|	but WITHOUT ANY WARRANTY; without even the implied warranty of			|
+|	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the			|
+|	GNU General Public License for more details.							|
+|																			|
+|	You should have received a copy of the GNU General Public License		|
+|	along with Foobar.  If not, see <http://www.gnu.org/licenses/>.			|
+|===========================================================================|
+|	> $Date$																|
+|	> $Revision$															|
+|	> $Author$																|
++---------------------------------------------------------------------------+
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DemonServer
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+}
