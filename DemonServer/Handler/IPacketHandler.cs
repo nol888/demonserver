2f800d5848b44bb9dafb93e3bbbba7797b507c2b
nol888 <nol888@cfc62999-394d-0410-9d05-3f146b4bb521>
2008-09-22 20:36:14 +0000

    Add packet handler base and user basics - more on this later.

---
+++ DemonServer/Handler/IPacketHandler.cs
@@ -0,0 +1,42 @@
+﻿/*
++---------------------------------------------------------------------------+
+|	Demon - dAmn Emulator													|
+|===========================================================================|
+|	Copyright © 2008 Nol888													|
+|===========================================================================|
+|	This file is part of Demon.												|
+|																			|
+|	Demon is free software: you can redistribute it and/or modify			|
+|	it under the terms of the GNU Affero General Public License as			|
+|	published by the Free Software Foundation, either version 3 of the		|
+|	License, or (at your option) any later version.							|
+|																			|
+|	This program is distributed in the hope that it will be useful,			|
+|	but WITHOUT ANY WARRANTY; without even the implied warranty of			|
+|	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the			|
+|	GNU Affero General Public License for more details.						|
+|																			|
+|	You should have received a copy of the GNU Affero General Public License|
+|	along with this program.  If not, see <http://www.gnu.org/licenses/>.	|
+|																			|
+|===========================================================================|
+|	> $Date: 2008-09-21 13:57:19 -0400 (Sun, 21 Sep 2008) $
+|	> $Revision: 3 $
+|	> $Author: nol888 $
++---------------------------------------------------------------------------+
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DemonServer.User;
+
+namespace DemonServer.Handler
+{
+	interface IPacketHandler
+	{
+		void handlePacket(string Packet, DAmnUser user);
+
+		bool validateState(DAmnUser user);
+	}
+}
---
+++ DemonServer/Handler/PacketProcessor.cs
@@ -0,0 +1,49 @@
+﻿/*
++---------------------------------------------------------------------------+
+|	Demon - dAmn Emulator													|
+|===========================================================================|
+|	Copyright © 2008 Nol888													|
+|===========================================================================|
+|	This file is part of Demon.												|
+|																			|
+|	Demon is free software: you can redistribute it and/or modify			|
+|	it under the terms of the GNU Affero General Public License as			|
+|	published by the Free Software Foundation, either version 3 of the		|
+|	License, or (at your option) any later version.							|
+|																			|
+|	This program is distributed in the hope that it will be useful,			|
+|	but WITHOUT ANY WARRANTY; without even the implied warranty of			|
+|	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the			|
+|	GNU Affero General Public License for more details.						|
+|																			|
+|	You should have received a copy of the GNU Affero General Public License|
+|	along with this program.  If not, see <http://www.gnu.org/licenses/>.	|
+|																			|
+|===========================================================================|
+|	> $Date$
+|	> $Revision$
+|	> $Author$
++---------------------------------------------------------------------------+
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DemonServer.Handler
+{
+	public class PacketProcessor
+	{
+		#region Private Properties
+		private static PacketProcessor _instance;
+		private Dictionary<string, IPacketHandler> _handlers;
+		#endregion
+
+		private PacketProcessor()
+		{
+			this._handlers = new Dictionary<string, IPacketHandler>();
+
+			// Register the handlers here.
+		}
+	}
+}
